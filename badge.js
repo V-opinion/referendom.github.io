2093428b95759d7b63e741edc8d7da9a42962672
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-01-06 16:35:53 -0500

    Update badge.js

--- badge.js
+++ badge.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('No root element found');
+}
+
 (function () {
   // Find the current <script> tag that loaded badge.js
   var currentScript = document.currentScript || (function () {
